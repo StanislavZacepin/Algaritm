08235ce4cb5645680d2a77e0527860fd1b053794
TouchPad <olsher258@mail.ru>
2021-03-31 20:11:55 +0300

    Добавьте файлы проекта.

---
+++ Algaritm_Dz/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algaritm_Dz.dz1_1;
+
+namespace Algaritm_Dz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dz1_1.Dz1();
+
+
+        }
+    }
+}
