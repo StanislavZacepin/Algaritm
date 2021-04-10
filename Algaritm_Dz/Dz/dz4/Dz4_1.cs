d4d15c7c769c74b423841a24173e3b51fe9873a7
TouchPad <olsher258@mail.ru>
2021-04-10 11:01:10 +0300

    Очень интересное задания //  Зацепин С.С.  1. * Количество маршрутов с препятствиями.Реализовать чтение массива с препятствием и нахождение количество маршрутов. //Например, карта:

--- Algaritm_Dz/Dz/dz4/Dz4_1.cs
+++ Algaritm_Dz/Dz/dz4/Dz4_1.cs
@@ -20,17 +20,30 @@ namespace Algaritm_Dz.Dz.dz4
         }
          static int CreatsMaps(int n , int k)
         {
-            int [,] Maps = new int  [n + 1, n + 1];
-            for (int i = 0; i <= n; i++)
+            int rn;
+            Random rnd = new Random();
+            int [,] Maps = new int  [n+1, k+1];
+            for (int j = 0; j <= k; j++)
             {
-                Maps[i, 0] = 1;
-                Maps[i, i] = 1;
-                for (int j = 1; j < i; j++)
+                rn = rnd.Next(0, 3);
+                if (rn == 0) Maps[0, j] = 0; 
+                else  Maps[0, j] = 1;
+               
+            }
+            printer(Maps, n, k);
+            for (int i = 1; i < n; i++)
+            {
+                rn = rnd.Next(0, 3);
+                if (rn == 0) Maps[i,0] = 0;
+                else Maps[i, 0] = 1;
+                for (int j = 1; j < k; j++)
                 {
-                    Maps[i, j] = Maps[i - 1, j - 1] + Maps[i - 1, j];
+                    Maps[i, j] = Maps[i, j - 1] + Maps[i - 1, j];
                 }
                 
-            }
+             }
+                
+            
             printer(Maps,n,k);
             return Maps[n,k];
         }
@@ -40,9 +53,13 @@ namespace Algaritm_Dz.Dz.dz4
             for (int i = 0; i < n; i++)
             {
                 for (int j = 0; j < m; j++)
-                    Console.Write(String.Format("{0,3}", mass[i, j]));
+                {
+                    Console.Write(String.Format(" {0,3} ", mass[i, j]));
+                    
+                }
                 Console.WriteLine();
             }
+            Console.WriteLine("------------------------------");
         }
     }
 }
