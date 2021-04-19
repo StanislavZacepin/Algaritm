7acc040b5d831221798d11d6c516a8eb6a690b6d
TouchPad <olsher258@mail.ru>
2021-04-19 20:08:27 +0300

    3. Написать программу, которая определяет, является ли введенная скобочная последовательность правильной. Примеры правильных скобочных выражений: (), ([])(), {}(), ([{}]), неправильных — )(, ())({), (, ])}), ([(]) для скобок [,(,{. Например: (2+(2*2)) или [2/{5*(4+7)}]

---
+++ Algaritm_Dz/Dz/dz5/Dz5_3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algaritm_Dz.Dz.dz5
+{
+   static public class Dz5_3
+    {
+        static public void Start()
+        {
+            Stack<char> stack = new Stack<char>();
+            Console.Write("Введите строку: ");
+            string str = Console.ReadLine();
+
+            for (int i = 0; i < str.Length; i++)
+                if (str[i] == '(')
+                    stack.Push('(');
+                else if (str[i] == ')' & stack.Count > 0)
+                    stack.Pop();
+
+            Console.WriteLine((stack.Count == 0) ? "Выражение верное." : "Выражение не верное!");
+            Console.ReadKey();
+        }
+    }
+}
